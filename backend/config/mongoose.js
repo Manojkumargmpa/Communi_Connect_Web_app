70303caddad048eaf8b9db38fea8767716956610
manoj <manojkumargompa@gmail.com>
2023-10-02 14:03:39 +0530

    modified announcements frontend and added filters

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -7,6 +7,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/communi', {
 mongoose.connect('');
 
 
+
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
